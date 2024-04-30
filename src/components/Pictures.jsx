9fc98be1cd1def00c12e6dbf2ee6b44a36fee400
Aishat Agbolade <aishaagbolade@gmail.com>
2024-04-30 13:28:06 +0100

    removed uuid from pictures

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 import { useDictionary } from "../components/DictionaryContext";
 import Spinner from "./Spinner";
 
